0d6bceb5a10918b80dad083dcf6d68711f9ef06b
momocloud <wangkunyusimi@gmail.com>
2021-02-03 21:23:05 +0100

    complete edit page design

---
+++ pages/edit/edit.vue
@@ -0,0 +1,219 @@
+<template>
+	<view style="background-color: #f0f0f0;" :style="'height: ' + this.windowH + 'px'">
+		<view style="background-color: white; height: 100rpx; display: flex;">
+			<image src='../../static/back.svg' class=titleBtn @click="backToHome"></image>
+			<view class=title>EDIT</view>
+			<image src='../../static/tick.svg' class=titleBtn style="margin-left: 78%;" @click="addFindsh"></image>
+		</view>
+		
+		<view style="text-align: center; font-size: 36rpx; padding-top: 5%; color: #808080; font-size: 30px;">
+			{{item.name}}
+		</view>
+		
+		<!-- Goal -->
+		<view class="tintText">Goal :</view>		
+		<view class=itemBkg style="margin-top: 5%; display: flex;">
+			<input placeholder="4" type="number" class="setIn" style="background-color: #c9c9c9; width: 20%; border-radius: 27px; text-align: center; margin-left: 2%;" />
+			<input placeholder="km" type="text" class="setIn" style="background-color: #c9c9c9; width: 25%; border-radius: 27px; text-align: center; margin-left: 5%;" />
+			<view class="setIn" style="margin-left: 5%;">per</view>
+			<view style="width: 25%; position: absolute; margin-left: 70%; padding-top: 7rpx;">
+				<xfl-select-white 
+					:list="unitList"
+					:clearable="false"
+					:showItemNum="5" 
+					:listShow="false"
+					:isCanInput="false"  
+					:style_Container="'height: 80rpx; font-size: 20px; '"
+					:placeholder = "'placeholder'"
+					:initValue="'day'"
+					:selectHideType="'hideAll'"
+				>
+				</xfl-select-white>
+			</view>
+		</view>
+
+		<!-- Reminder -->
+		<view class="tintText">Reminder :</view>
+		<view class=itemBkg style="margin-top: 5%; display: flex;">
+			<view class="setIn" style="margin-left: 5%;">Don't remind me</view>
+			<switch @change="changeRe" style="margin-left: 30%; margin-top: 2%;" color="#c9c9c9"></switch>
+		</view>
+		<view class=itemBkg style="margin-top: 5%; display: flex;">
+			<view class="setIn" style="margin-left: 5%;">Remind me at</view>
+			<date-time-picker ref='date-time' type='hour-minute' @change='dateTimeChange'></date-time-picker>
+			<view class="setIn" style="margin-left: 2%; background-color: #c9c9c9; border-radius: 27px; width: 17%; text-align: center; margin-bottom: 2.5%;" @click="clickChangeTime">{{remindTime}}</view>
+			<view class="setIn" style="margin-left: 2%;">every</view>
+			<view style="width: 26%; position: absolute; margin-left: 73%; padding-top: 7rpx;">
+				<xfl-select-white 
+					:list="remindTimeList"
+					:clearable="false"
+					:showItemNum="8" 
+					:listShow="false"
+					:isCanInput="false"  
+					:style_Container="'height: 80rpx; font-size: 20px; '"
+					:placeholder = "'placeholder'"
+					:initValue="'day'"
+					:selectHideType="'hideAll'"
+				>
+				</xfl-select-white>
+			</view>
+		</view>
+		
+		<!-- Tag -->
+		<view class="tintText">Tag :</view>
+		<view class=itemBkg style="margin-top: 5%;">
+			<input style="text-align: center; font-size: 36rpx; padding-top: 3%;" placeholder="Tap to enter its tag!" @input="onTagInput" />
+		</view>
+		
+		<!-- Color -->
+		<view class="tintText">Color :</view>
+		<view style="background-color: white; height: 200rpx; ">
+			<view style="display: flex;">
+				<view style="background-color: #ee3f4d;" class="colorBall"></view>
+				<view style="background-color: #f07c82;" class="colorBall"></view>
+				<view style="background-color: #c35691;" class="colorBall"></view>
+				<view style="background-color: #8076a3;" class="colorBall"></view>
+				<view style="background-color: #a7a8bd;" class="colorBall"></view>
+				<view style="background-color: #93b5cf;" class="colorBall"></view>
+			</view>
+			<view style="display: flex;">
+				<view style="background-color: #22a2c3;" class="colorBall"></view>
+				<view style="background-color: #2c9678;" class="colorBall"></view>
+				<view style="background-color: #bec936;" class="colorBall"></view>
+				<view style="background-color: #eed045;" class="colorBall"></view>
+				<view style="background-color: #ffa60f;" class="colorBall"></view>
+				<view style="background-color: #de7622;" class="colorBall"></view>
+			</view>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	import DateTimePicker from '../../components/bory-dateTimePicker/bory-dateTimePicker.vue'
+	export default {
+		
+		components:{DateTimePicker},
+		data() {
+			return {
+				windowH: undefined,
+				nameValue: undefined,
+				unitList: ['day', 'week', 'month'],
+				remindTime: '00:00',
+				remindTimeList: ['day', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+				itemList: undefined,
+				item: undefined,
+			}
+		},
+		methods: {
+			//返回上一层
+			backToHome() {
+				uni.navigateBack({
+					delta: 1,
+					animationType:'pop-out',
+					animationDuration: 200
+				});
+			},
+			
+			//完成
+			addFindsh() {
+				console.log(this.nameValue);
+				uni.navigateBack({
+					delta: 1,
+					animationType:'pop-out',
+					animationDuration: 200
+				});
+			},
+			
+			//键入名字时将数据保存到配置文件
+			onNameInput(e) {
+				this.nameValue = e.target.value;
+			},
+			
+			changeRe(e) {
+				console.log(e.target.value)
+			},
+			
+			dateTimeChange(e) {
+				this.remindTime = e;
+			},
+			
+			clickChangeTime() {
+				this.$refs['date-time'].show();
+			},
+			
+			onTagInput(e) {
+				console.log(e.target.value);
+			}
+			
+		},
+		
+		
+		
+		onLoad: function(parm) {
+			this.windowH = uni.getSystemInfoSync().windowHeight;
+			this.itemList = uni.getStorageSync('data');
+			this.item = this.itemList[parm.id];
+		},
+		
+
+	}
+</script>
+
+<style>
+	.title {
+		margin-top: 3%;
+		text-align: center;
+		font-size: 24px;
+		color: #000000;
+		width: 150rpx;
+		position: absolute;
+		margin-left: 40%;
+	}
+	
+	.titleBtn {
+		width: 10%;
+		height: 70%;
+		margin-top: 2%;
+	}
+	
+	.setBkg {
+		background-color: white;
+		height: 50px;
+		width: 100%;
+		display: flex;
+	}
+	
+	.itemBkg {
+		height: 50px;
+		background-color: white;
+	}
+	
+	.setIn {
+		margin-top: 21rpx;
+		font-size: 20px;
+		color: #000000;
+	}
+	
+	.itemBkgHalf {
+		height: 50px;
+		background-color: white;
+		width: 48%;
+	}
+	
+	.tintText {
+		font-size: 20px;
+		margin-left: 2%;
+		margin-top: 10%;
+	}
+	
+	.colorBall {
+		width: 40px;
+		height: 40px;
+		border-radius: 50%;
+		margin-left: 3.5%;
+	    margin-right: 3.5%;
+		margin-top: 2%;
+		margin-bottom: 2%;
+	}
+</style>
